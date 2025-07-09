f33f98862c24d89f1af1a76e1e34fbd94e5efc7e
Fabrigiorda <fgiordanelli@alumno.huergo.edu.ar>
2025-07-09 18:26:33 -0300

    LISTO

--- djangotutorial/mysite/mysite/urls.py
+++ djangotutorial/mysite/mysite/urls.py
@@ -1,15 +1,12 @@
+
+
 from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings 
+
+from django.urls import path, include
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += [
-        path('__debug__/', include('debug_toolbar.urls')),
-    ]
